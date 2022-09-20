a40bbf2e5b65774ce3233fc59d86d22a97f1e4ae
Hiroshi.Nakai <hrsi.ad@gmail.com>
2022-09-20 17:22:15 +0900

    openGL 道場 第1回　終了

--- ソース.cpp
+++ ソース.cpp
@@ -1,3 +1,25 @@
+#include "glut.h"
+
+
+//void (GLUTCALLBACK* func)(void
+
+void display(void) {
+    glutWireTeapot(1);
+    glFlush();
+
+}
+
 int main(int argc, char* argv[]) {
+    
+    glutInit(
+        &argc,  // int *argcp
+        argv);  // char **argv
+
+    glutInitWindowPosition(640, 0);
+    glutInitWindowSize(640, 640);
+
+    glutCreateWindow("Title");
+    glutDisplayFunc(display);
+    glutMainLoop();
 
 }
