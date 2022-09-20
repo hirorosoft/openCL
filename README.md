df48986174f327aa9b2c0683897c3747234fb8fb
Hiroshi.Nakai <hrsi.ad@gmail.com>
2022-09-20 20:50:12 +0900

    openGL 道場2 完了

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # openCL
+
+セットアップ
+https://gameprogrammingunit.web.fc2.com/gl/index.html
+
+
+
+TIPS
+
+Viasul Studio 2022
+
+コード整形
+選択しているファイル全体で整形する場合
+Ctrl + K 
+Ctrl + D
--- ソース.cpp
+++ ソース.cpp
@@ -1,25 +1,41 @@
 #include "glut.h"
 
-
-//void (GLUTCALLBACK* func)(void
-
 void display(void) {
-    glutWireTeapot(1);
-    glFlush();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glRotatef(1, 0, 0, 1);
+	glutWireTeapot(1);
+
+	// glFlush()
+	glutSwapBuffers();
 
 }
 
+
+// アイドル関数
+void Idle(void) {
+	glutPostRedisplay();
+
+}
+
+
 int main(int argc, char* argv[]) {
-    
-    glutInit(
-        &argc,  // int *argcp
-        argv);  // char **argv
 
-    glutInitWindowPosition(640, 0);
-    glutInitWindowSize(640, 640);
+	// 初期化
+	glutInit(&argc, argv);
 
-    glutCreateWindow("Title");
-    glutDisplayFunc(display);
-    glutMainLoop();
+	glutInitDisplayMode(GL_DOUBLE);
+
+	// 以下、無くてもOK
+	glutInitWindowPosition(640, 0);
+	glutInitWindowSize(640, 640);
+
+	glutCreateWindow("Title");
+
+	// コールバック関数登録
+	glutDisplayFunc(display);
+	glutIdleFunc(Idle);
+
+
+	glutMainLoop();
 
 }
